3cadd084bf5ebfd72a0fbcccafd9e4045d0c8875
Vincent Verhoeven <verhoevenv@gmail.com>
2016-10-02 14:26:46 +0200

    Add a more readable alternative for map2 using for-comprehension
    Note that the for-comprehension is just syntactic sugar for the flatmap/map implementation!

--- src/main/scala/chapter4/Ex3.scala
+++ src/main/scala/chapter4/Ex3.scala
@@ -3,4 +3,11 @@ package chapter4
 object Ex3 {
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a flatMap (av => b map (bv => f(av, bv)))
+
+
+  def map2_FC[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for {
+      av <- a
+      bv <- b
+    } yield f(av, bv)
 }
