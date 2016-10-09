7ff48b241649ca20366e8f2e5db82d1769d77429
Vincent Verhoeven <verhoevenv@gmail.com>
2016-10-09 22:08:56 +0200

    Some proof that memoization works

--- src/test/scala/chapter5/Ex3Test.scala
+++ src/test/scala/chapter5/Ex3Test.scala
@@ -8,4 +8,26 @@ class Ex3Test extends FunSuite {
     assert(Stream(1, 2, 3, 4, 3, 2, 1).takeWhile(_ < 3).toList == List(1, 2))
   }
 
+  import Stream._
+
+  test("takeWhile evaluates only once") {
+    val inputStream: Stream[Int] = cons(maxOnce(1), cons(maxOnce(2), cons(never(3), empty)))
+    val filteredStream: Stream[Int] = inputStream.takeWhile(_ < 2)
+
+    assert(filteredStream.toList == List(1))
+  }
+
+  def maxOnce[A]: A => A = failAfter(1)
+  def never[A]: A => A = failAfter(0)
+
+  def failAfter[A](n: Int): A => A = {
+    var amount = 0
+    x => {
+      amount += 1
+      if(amount > n) {
+        fail()
+      }
+      x
+    }
+  }
 }
