08131988730505be70691ddcc919c7e4b0544847
Vincent Verhoeven <verhoevenv@gmail.com>
2016-12-14 20:00:24 +0100

    fix code compilation

--- src/test/scala/chapter6/Ex11Test.scala
+++ src/test/scala/chapter6/Ex11Test.scala
@@ -5,7 +5,7 @@ import org.scalatest.FunSuite
 class Ex11Test extends FunSuite {
 
   ignore("state machines") {
-    val initialState = Machine(locked = false, candies = 5, coins = 10)
+    val initialState : Machine = Machine(locked = false, candies = 5, coins = 10)
     val inputs: List[Input] = List(Coin, Turn, Coin, Turn, Coin)
 
     val ((coins, candies), endState): ((Int, Int), Machine) = Machine.simulateMachine(inputs)(initialState)
